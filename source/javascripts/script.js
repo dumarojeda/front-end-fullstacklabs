90d37b685fb4da8fe2ee7fb1f48c65a3943a35e3
Dumar Ojeda <dumar@kommit.co>
2018-06-21 02:54:26 -0500

    Add animation for active item and show back button

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,9 +1,20 @@
 var Global = function() {
   'use strict';
 
+  var itemActive =  function () {
+    $('.menu ul li a').click(function () {
+      $('#wrapper-menu').addClass('mode-minimal');
+      $('.back').addClass('active');
+    });
+    $('.back').click(function () {
+      $(this).removeClass('active');
+      $('#wrapper-menu').removeClass('mode-minimal');
+    });
+  }
+
   return {
     init : function () {
-      
+      itemActive();
     }
   }
 
