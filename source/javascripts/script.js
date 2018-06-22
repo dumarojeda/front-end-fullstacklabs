c43afef8fc2c3b2b6873a89ae476e3ad999d5413
Dumar Ojeda <dumarojeda@gmail.com>
2018-06-21 22:22:01 -0500

    Add bscis structure for profile

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -20,9 +20,37 @@ var Global = function() {
     });
   }
 
+  var itemMenuLineUp = function () {
+    $(".menu-lineup li a").click(function () {
+      $(".menu-lineup li a").removeClass("active");
+      $(this).toggleClass("active");
+    });
+
+    $(".menu-lineup li.profile").click(function () {
+      $("body").addClass("profile-active");
+    });
+
+    $(".menu-lineup li.lineup").click(function () {
+      $("body").removeClass("profile-active");
+    });
+  }
+
+  var teamMemberActive = function () {
+    $(".team ul li a").click(function (e) {
+      e.preventDefault();
+      var name = $(this).data('name');
+      var lastname = $(this).data('lastname');
+      var bg = $(this).data('bg');
+      $(".menu-lineup li.profile a").text(name + "'s Profile")
+      $(".menu-lineup li.stats a").text(name + "'s Stats")
+    })
+  }
+
   return {
     init : function () {
       itemActive();
+      itemMenuLineUp();
+      teamMemberActive();
     }
   }
 
